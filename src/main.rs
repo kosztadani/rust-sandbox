9bc94d8ac8a91899b9bdeb9fb9ba2f19b35b0820
Dániel Koszta <daniel.koszta@gmail.com>
2023-10-23 21:26:10 +0200

    Use hexadecimal machine code
    Change-Id: I2acae7bf38c25ec6df2d879974122f0207a3aee8

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ fn main() {
     let message = "Hello, World!\n";
     unsafe {
         asm!(
-        "mov rax, 1",
-        "mov rdi, 1",
-        "syscall",
+        // mov rax, 1
+        ".byte 0x48, 0xc7, 0xc0, 0x01, 0x00, 0x00, 0x00",
+        // mov rdi, 1
+        ".byte 0x48, 0xc7, 0xc7, 0x01, 0x00, 0x00, 0x00",
+        // syscall
+        ".byte 0x0F, 0x05",
         in("rsi") message.as_ptr(),
         in("rdx") message.len(),
         );
