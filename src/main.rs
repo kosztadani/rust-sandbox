9762ca8bb6ed9b6ea63d1a04fae3740b57e88352
Dániel Koszta <daniel.koszta@gmail.com>
2023-10-27 23:12:43 +0200

    Put the message in assembly too
    Change-Id: I923457a32b757fc9837fb4c246dab1aaff6ef87c

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 use std::arch::asm;
 
 fn main() {
-    let message = "Hello, World!\n";
     unsafe {
         asm!(
+        // jmp .+16 ; jump over the message
+        ".byte 0xeb, 0x0e",
+        // put the message right here in the code (nasty)
+        ".ascii \"Hello, World!\\n\"",
+        // lea rsi, [rip - 21] ; calculate pointer to message
+        ".byte 0x48, 0x8d, 0x35, 0xeb, 0xff, 0xff, 0xff",
+        // mov rdx, 14
+        ".byte 0x48, 0xc7, 0xc2, 0x0e, 0x00, 0x00, 0x00",
         // mov rax, 1
         ".byte 0x48, 0xc7, 0xc0, 0x01, 0x00, 0x00, 0x00",
         // mov rdi, 1
         ".byte 0x48, 0xc7, 0xc7, 0x01, 0x00, 0x00, 0x00",
         // syscall
         ".byte 0x0F, 0x05",
-        in("rsi") message.as_ptr(),
-        in("rdx") message.len(),
         );
     }
 }
