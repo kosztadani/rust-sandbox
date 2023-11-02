aef2a269bd1aeff6b6aedb6189b4e3fcdfee6e89
Dániel Koszta <daniel.koszta@gmail.com>
2023-11-02 07:34:27 +0100

    Improve comments
    Change-Id: I1861c28451119df184cec468608ed8680159d14d

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ fn main() {
         ".byte 0xeb, 0x0e",
         // put the message right here in the code (nasty)
         ".ascii \"Hello, World!\\n\"",
-        // lea rsi, [rip - 21] ; calculate pointer to message
+        // lea rsi, [rip - 21] ; calculate pointer to message (second argument)
         ".byte 0x48, 0x8d, 0x35, 0xeb, 0xff, 0xff, 0xff",
-        // mov rdx, 14
+        // mov rdx, 14 ; third argument (count)
         ".byte 0x48, 0xc7, 0xc2, 0x0e, 0x00, 0x00, 0x00",
-        // mov rax, 1
+        // mov rax, 1 ; syscall number: sys_write
         ".byte 0x48, 0xc7, 0xc0, 0x01, 0x00, 0x00, 0x00",
-        // mov rdi, 1
+        // mov rdi, 1 ; first argument: stdout
         ".byte 0x48, 0xc7, 0xc7, 0x01, 0x00, 0x00, 0x00",
         // syscall
         ".byte 0x0F, 0x05",
